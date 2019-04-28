8938667af8ec7f5f7267dde7c7070e628754e687
Md. Shakib Hassan <mshassan.dhk313@gmail.com>
2019-04-29 05:38:10 +0600

    backDrop close with onClick

--- src/components/UI/Backdrop/Backdrop.jsx
+++ src/components/UI/Backdrop/Backdrop.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CssClasses from './Backdrop.module.css';
 const backdrop = (props) => (
  
-    props.show ? <div className ={CssClasses.Backdrop} onclick={props.clicked}></div> :null 
+    props.show ? <div className ={CssClasses.Backdrop} onClick={props.clicked}></div> :null 
 );
 
 export default backdrop;
